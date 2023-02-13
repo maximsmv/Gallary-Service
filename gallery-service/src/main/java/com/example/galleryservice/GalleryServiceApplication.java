af1c66fef4a5269ed3cd5fccf997aa836cb1660c
maximsmv <maximsmv27@gmail.com>
2023-02-13 15:01:47 +0500

    Initial commit

---
+++ gallery-service/src/main/java/com/example/galleryservice/GalleryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.galleryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryServiceApplication.class, args);
+	}
+
+}
